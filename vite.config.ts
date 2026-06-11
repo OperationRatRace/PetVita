cb91c16a8f23923591af27ddaa1fcd8c3ee2c1d4
PrakGov <55034062+prak112@users.noreply.github.com>
2026-06-11 17:03:06 +0300

    debug: Remove base dir in defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/PetVita/',
+  // base: '/PetVita/',
   resolve: {
     alias: {
       // Alias @ to the src directory
